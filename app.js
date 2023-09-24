76eb17e442b55cbd2cd42186d3a40074ea18ed38
roushan-sheik <roushansheik@gmail.com>
2023-09-24 16:35:37 +0600

    code optimise by custom framework

--- app.js
+++ app.js
@@ -1,29 +1,54 @@
 const http = require("http");
 
+const student = [
+  {
+    name: "arifa moni",
+    email: "arifamoni@gmail.com",
+    dis: "khulna",
+    age: 20,
+  },
+  {
+    name: "rohana",
+    email: "arifamoni@gmail.com",
+    dis: "khulna",
+    age: 20,
+  },
+  {
+    name: "Tasmiya",
+    email: "arifamoni@gmail.com",
+    dis: "Jessore",
+    age: 22,
+  },
+  {
+    name: "Rohan",
+    email: "rohan@gmail.com",
+    dis: "khulna",
+    age: 25,
+  },
+];
+function sendResponse(
+  res,
+  { contentType = "application/json", body = {}, status = 200 }
+) {
+  res.writeHead(status, { "Content-Type": contentType });
+  res.write(JSON.stringify(body));
+  res.end();
+}
+
 const server = http.createServer((req, res) => {
   if (req.url === "/") {
-    res.writeHead(200, { "Content-Type": "application/json" });
-    res.write(
-      JSON.stringify({
-        name: "arifa",
-        email: "arifa@gmail.com",
-        districk: "Khulna ",
-      })
-    );
-    res.end();
+    sendResponse(res, { body: { msg: "This is Home route" } });
+  } else if (req.url === "/students") {
+    sendResponse(res, { body: student });
   } else {
-    res.writeHead(404, { "Content-Type": "application/json" });
-    res.write(
-      JSON.stringify({
-        msg: "Resource not found",
-      })
-    );
-    res.end();
+    sendResponse(res, {
+      status: 404,
+      body: { msg: "404 route dose not exists" },
+    });
   }
 });
 
 let port = process.env.port || 3000;
-
 server.listen(port, () => {
-  console.log(`Server is running on port:http://localhost:${port}`);
+  console.log(`Server is running on port http:localhost:${port}`);
 });
